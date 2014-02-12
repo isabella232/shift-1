8857be1a70c2bd7ab6c4f7a5366eaf8016ee483d
kirkbushell <torm3nt@gmail.com>
2014-02-12 16:32:01 +0000

    Added circleci test config.

--- tests/Library/BaseControllerTest.php
+++ tests/Library/BaseControllerTest.php
@@ -42,4 +42,37 @@ class BaseControllerTest extends PHPUnit_Framework_TestCase
 
 		$this->controller->store();
 	}
+
+	public function testShowShouldReturnTheResource()
+	{
+		$id = 1;
+		$model = 'returned resource';
+
+		$this->mockRepository->shouldReceive('find')->with($id)->andReturn($model);
+
+		$this->controller->show($id);
+	}
+
+	public function testUpdateShouldEditExistingRecord()
+	{
+		$params = ['param' => 'value'];
+		$model = 'returned model';
+		$id = 1;
+
+		$this->mockRequest->shouldReceive('input')->andReturn($params);
+
+		$this->mockRepository->shouldReceive('update')->with($id, $params);
+
+		$this->controller->update($id);
+	}
+
+	public function testDestroyShouldReturnTheDeletedResource()
+	{
+		$id = 1;
+		$model = 'returned resource';
+
+		$this->mockRepository->shouldReceive('delete')->with($id)->andReturn($model);
+
+		$this->assertEquals( $this->controller->destroy($id), $model );
+	}
 }
