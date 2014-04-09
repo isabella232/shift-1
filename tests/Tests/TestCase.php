388e9c1e9510e4426253d29bda613b0ffbf86640
kirkbushell <torm3nt@gmail.com>
2014-04-09 17:48:02 +0100

    integrated tests working.

--- tests/Tests/TestCase.php
+++ tests/Tests/TestCase.php
@@ -17,4 +17,35 @@ class TestCase extends \Orchestra\Testbench\TestCase
 	{
 		return array('Tectonic\Shift\ShiftServiceProvider');
 	}
+
+	/**
+	 * Define environment setup.
+	 *
+	 * @param  Illuminate\Foundation\Application    $app
+	 * @return void
+	 */
+	protected function getEnvironmentSetUp($app)
+	{
+		// reset base path to point to our package's src directory
+		$app['path.base'] = __DIR__ . '/../../';
+
+		$app['config']->set('database.default', 'testbench');
+		$app['config']->set('database.connections.testbench', array(
+			'driver'   => 'sqlite',
+			'database' => ':memory:',
+			'prefix'   => ''
+		));
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$artisan = $this->app->make('artisan');
+
+		$artisan->call('migrate', [
+			'--database' => 'testbench',
+			'--path'     => 'migrations'
+		]);
+	}
 }
