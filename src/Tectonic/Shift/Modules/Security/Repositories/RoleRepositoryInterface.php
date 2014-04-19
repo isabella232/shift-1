64c22e4c2eac68f338db801dfad56a6e1c0b24e2
kirkbushell <torm3nt@gmail.com>
2014-04-19 14:07:03 +0100

    Implemented the new method on the role repository interface.

--- src/Tectonic/Shift/Modules/Security/Repositories/RoleRepositoryInterface.php
+++ src/Tectonic/Shift/Modules/Security/Repositories/RoleRepositoryInterface.php
@@ -6,5 +6,10 @@ use Tectonic\Shift\Library\Contracts\BaseRepositoryInterface;
 
 interface RoleRepositoryInterface extends BaseRepositoryInterface
 {
-	
+    /**
+     * Should return a single record for the default role.
+     *
+     * @return mixed
+     */
+    public function getByDefault();
 }
