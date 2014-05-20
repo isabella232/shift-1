e46e990758b25bed97e6e0a6059cd9f01baba10e
kirkbushell <torm3nt@gmail.com>
2014-05-20 09:45:12 +0100

    Added a new integrated test for defining new default roles.

--- tests/Api/Security/RolesTest.php
+++ tests/Api/Security/RolesTest.php
@@ -20,7 +20,7 @@ class RolesTest extends Tests\TestCase
             'account_id' => 1,
             'access'     => 1,
             'name'       => 'Test Role',
-            'default'    => true
+            'default'    => false
         ];
 
         // Act
@@ -30,6 +30,35 @@ class RolesTest extends Tests\TestCase
         // Assert
         $this->assertResponseOk();
         $this->assertSame($data['name'], $newRole->name);
+    }
+
+    public function testSetDefaultRole()
+    {
+        $existingRoleData = [
+            'account_id' => null,
+            'access' => 1,
+            'default' => true,
+            'name' => 'Existing role'
+        ];
+
+        $this->roleModel->create($existingRoleData);
+
+        $newRoleData = [
+            'account_id' => null,
+            'access' => 1,
+            'default' => true,
+            'name' => 'New default role'
+        ];
+
+        // Act
+        $this->call('POST', 'roles', $newRoleData);
+
+        // Assert
+        $newDefaultRole = $this->roleModel->whereDefault(true)->get();
+
+        $this->assertResponseOk();
+        $this->assertCount(1, $newDefaultRole);
+        $this->assertSame($newRoleData['name'], $newDefaultRole[0]->name);
     }
 
     public function testGetAllRoles()
