fbf0efc1653e1c3d0277e981d034a470e50096ed
kirkbushell <torm3nt@gmail.com>
2014-09-16 20:19:02 +0100

    Account repository interface updated.

--- Shift/Modules/Accounts/Repositories/AccountRepositoryInterface.php
+++ Shift/Modules/Accounts/Repositories/AccountRepositoryInterface.php
@@ -6,5 +6,19 @@ use Tectonic\Shift\Library\Support\Database\RepositoryInterface;
 
 interface AccountRepositoryInterface extends RepositoryInterface
 {
+	/**
+	 * Searches for a domain, should throw an exception if it fails.
+	 *
+	 * @param $domains
+	 * @return mixed
+	 */
 	public function requireByDomain($domains);
+
+	/**
+	 * Searches for a domain, but simply returns the result.
+	 *
+	 * @param $domains
+	 * @return mixed
+	 */
+	public function getByDomain($domains);
 }
