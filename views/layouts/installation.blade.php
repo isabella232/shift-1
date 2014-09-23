dc39149de63cbc4b62ce7fec738be1ff337b2a97
kirkbushell <torm3nt@gmail.com>
2014-09-23 12:45:47 +0100

    Deleted some rather old assets.

--- views/layouts/installation.blade.php
+++ views/layouts/installation.blade.php
@@ -6,6 +6,34 @@
     @endforeach
 </head>
 <body>
-    <div>@yield('content')</div>
+    <header id="header">
+        <div class="container">
+            <a href="" class="logo"></a>
+            <div id="control-panel" user-panel ng-show="user"></div>
+            <div id="control-panel" ng-hide="user">
+                <ul class="horizontal">
+                    <li>Have an account? <a href="/">Login</a>.</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <nav id="navigation">
+        <div class="container pad-on-handheld">
+            <ul class="horizontal menu" top-menu></ul>
+        </div>
+    </nav>
+
+    <section id="content">
+        <div
+            @yield('content')
+        </div>
+    </section>
+
+    <div id="footer-links">
+        <div class="container">
+            <footer-links input="footerLinks"></footer-links>
+        </div>
+    </div>
 </body>
 </html>
