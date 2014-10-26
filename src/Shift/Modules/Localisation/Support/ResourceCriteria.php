ba72d239f2b10c6809933edb3ce258f2ce014ee5
kirkbushell <torm3nt@gmail.com>
2014-10-26 17:31:55 +0000

    Wrote tests for the ResourceCriteria class.

--- src/Shift/Modules/Localisation/Support/ResourceCriteria.php
+++ src/Shift/Modules/Localisation/Support/ResourceCriteria.php
@@ -1,6 +1,14 @@
 <?php
 namespace Tectonic\Shift\Modules\Localisation\Support;
-
+/**
+ * Class ResourceCriteria
+ *
+ * Used by the localisation repository to search by resources and their IDs. This class basically
+ * ensures that a given format is always provided and available for those search conditions (rather
+ * than by passing a multi-dimensional array around.
+ *
+ * @package Tectonic\Shift\Modules\Localisation\Support
+ */
 class ResourceCriteria
 {
     /**
@@ -10,6 +18,11 @@ class ResourceCriteria
      */
     private $resources = [];
 
+    /**
+     * Register a new resource.
+     *
+     * @param $resource
+     */
     public function addResource($resource)
     {
         if (!isset($this->resources[$resource])) {
@@ -17,16 +30,36 @@ class ResourceCriteria
         }
     }
 
+    /**
+     * Register a new id for a given resource.
+     *
+     * @param $resource
+     * @param $id
+     */
     public function addId($resource, $id)
     {
+        if (!isset($this->resources[$resource])) {
+            throw new \Exception("Resource [$resource] does not exist. Make sure you've registered it first via ResourceCriteria::addResource('$resource').'");
+        }
         $this->resources[$resource][] = $id;
     }
 
+    /**
+     * Return all resources and their ids.
+     *
+     * @return array
+     */
     public function getResources()
     {
         return array_keys($this->resources);
     }
 
+    /**
+     * Return all ids for a given resource.
+     *
+     * @param $resource
+     * @return mixed
+     */
     public function getIds($resource)
     {
         return $this->resources[$resource];
