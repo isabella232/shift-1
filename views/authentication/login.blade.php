22c11f0ad7c0c0e53afee38e73ce29f7c09d0d6b
JasonMortonNZ <jasonlmorton@gmail.com>
2014-12-02 12:00:04 +1300

    Update log in form

--- views/authentication/login.blade.php
+++ views/authentication/login.blade.php
@@ -16,41 +16,47 @@
 
     <div class="container">
 
-        <h1>Log In</h1>
+        <h1>Log in</h1>
 
         <!-- Login form -->
-        {{ Form::open(['action' => 'Tectonic\Shift\Controllers\AuthenticationController@login']) }}
+        {{ Form::open(['action' => 'Tectonic\Shift\Controllers\AuthenticationController@login', 'class' => 'vertical well']) }}
 
             <div class="control">
-                <div class="control-label forty">
+                <div class="control-label">
                     {{ Form::label('email', 'Email') }}
                 </div>
-                <div class="control-field sixty">
+                <div class="control-field">
                     {{ Form::email('email') }}
                 </div>
             </div>
 
             <div class="control">
-                <div class="control-label forty">
+                <div class="control-label">
                     {{ Form::label('password', 'Password') }}
                 </div>
-                <div class="control-field sixty">
+                <div class="control-field">
                     {{ Form::password('password') }}
                 </div>
             </div>
 
             <div class="control">
-                <div class="control-label forty">
+                <div class="control-label">
                     {{ Form::label('remember', 'Remember me') }}
                 </div>
-                <div class="control-field sixty">
+                <div class="control-field">
                     {{ Form::checkbox('remember') }}
                 </div>
             </div>
 
             <div class="control">
                 <div class="control-field">
-                    <input type="submit" value="Log In">
+                    <a href="#">Have you forgotten you password?</a>
+                </div>
+            </div>
+
+            <div class="control">
+                <div class="control-field">
+                    <input type="submit" value="Log in">
                 </div>
             </div>
 
