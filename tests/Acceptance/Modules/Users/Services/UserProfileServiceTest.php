7e75176bdc96adf46bdf3d225a052759cb31d9e6
JasonMortonNZ <jasonlmorton@gmail.com>
2014-12-05 09:36:44 +1300

    Added a couple of acceptance tests for UserProfileService

--- tests/Acceptance/Modules/Users/Services/UserProfileServiceTest.php
+++ tests/Acceptance/Modules/Users/Services/UserProfileServiceTest.php
@@ -1,9 +1,12 @@
 <?php
 namespace Tests\Acceptance\Modules\Users\Services;
 
+use Mockery as m;
+use Tectonic\Shift\Modules\Identity\Users\Contracts\UserRepositoryInterface;
 use Tests\AcceptanceTestCase;
 use Illuminate\Support\Facades\App;
 use Tectonic\Shift\Modules\Identity\Users\Services\UserProfileService;
+use Tectonic\Shift\Modules\Identity\Users\Observers\UserProfileResponder;
 
 class UserProfileServiceTest extends AcceptanceTestCase
 {
@@ -14,7 +17,65 @@ class UserProfileServiceTest extends AcceptanceTestCase
         $this->service = App::make(UserProfileService::class);
     }
 
-    public function testOnValidationFailure(){}
+    public function testOnValidationFailure()
+    {
+        // Arrange
+        $user = $this->createTempUser();
+        $observer = m::spy(UserProfileResponder::class);
+        $testData = [
+            'firstName' => 'Test',
+            'lastName'  => 'User',
+            'email'     => 'email.address.dev', // Invalid email address should cause validation exception
+            'password'  => '',
+            'passwordConfirmation'  => '',
+        ];
 
-    public function testOnSuccess(){}
+        // Act
+        $this->service->updateProfile($user, $testData, $observer);
+
+        // Assert.
+        $observer->shouldHaveReceived('onValidationFailure')->once();
+
+        $resultUser = App::make(UserRepositoryInterface::class)->getOneBy('id', $user->id);
+        $this->assertNotSame($resultUser->email, $testData['email']);
+    }
+
+    public function testOnSuccess()
+    {
+        // Arrange
+        $user = $this->createTempUser();
+        $observer = m::spy(UserProfileResponder::class);
+        $testData = [
+            'firstName' => 'Test',
+            'lastName'  => 'User',
+            'email'     => 'new-email@address.dev',
+            'password'  => '',
+            'passwordConfirmation'  => '',
+        ];
+
+        // Act
+        $this->service->updateProfile($user, $testData, $observer);
+
+        // Assert.
+        $observer->shouldHaveReceived('onSuccess')->once();
+
+        $resultUser = App::make(UserRepositoryInterface::class)->getOneBy('id', $user->id);
+        $this->assertSame($resultUser->email, $testData['email']);
+    }
+
+    private function createTempUser()
+    {
+        $userRepository = App::make(UserRepositoryInterface::class);
+
+        $user = $userRepository->getNew();
+
+        $user->first_name = "Test";
+        $user->last_name = "User";
+        $user->email = "email@address.dev";
+        $user->password = 'password';
+
+        $userRepository->save($user);
+
+        return $userRepository->getByEmail($user->email);
+    }
 }
