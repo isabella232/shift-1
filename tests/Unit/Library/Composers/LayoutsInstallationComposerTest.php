16edeb8aabf1849002e78aae66137cd0ad21c39d
kirkbushell <torm3nt@gmail.com>
2014-12-30 02:01:05 +0000

    Layouts installation composer test written.

---
+++ tests/Unit/Library/Composers/LayoutsInstallationComposerTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit\Library\Composers;
+
+use Asset;
+use Mockery as m;
+use Tectonic\Shift\Library\Composers\AssetsComposer;
+use Tectonic\Shift\Library\Composers\LayoutsInstallationComposer;
+
+class LayoutsInstallationComposerTest extends \Tests\UnitTestCase
+{
+	public function testSetsUpAssets()
+    {
+        $shiftAssetContainer = m::spy('assetContainer');
+
+        Asset::shouldReceive('container')->once()->with('shift')->andReturn($shiftAssetContainer);
+
+        $composer = new LayoutsInstallationComposer;
+        $composer->compose();
+
+        $shiftAssetContainer->shouldHaveReceived('add')->twice();
+    }
+}
